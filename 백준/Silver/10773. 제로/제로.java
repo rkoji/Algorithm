1a2751322e608912353d96a48c010af5a5822e78
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-11-03 15:03:11 +0900

    [Silver IV] Title: 제로, Time: 856 ms, Memory: 112772 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack();
+        int K = sc.nextInt();
+        for (int i = 0; i < K; i++) {
+            int num = sc.nextInt();
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        int sum = 0;
+        for (int num : stack) {
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
