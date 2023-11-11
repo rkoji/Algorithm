e57f8f5e17849b2b4661a7197f83bd730371027d
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-11-11 21:30:23 +0900

    [Bronze II] Title: 숫자, Time: 1260 ms, Memory: 30708 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+
+
+        if(A > B){
+            System.out.println(A-B-1);
+            for(long i = B+1 ; i <A;i++){
+                System.out.print(i+" ");
+            }
+        }else if(B > A){
+            System.out.println(B-A-1);
+            for(long i = A+1;i<B;i++){
+                System.out.print(i+" ");
+            }
+        }else{
+            System.out.print(0);
+        }
+    }
+}
