db712d4d6bedb2d41ee601b99085d8d74bf0d585
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-11-29 20:48:02 +0900

    Create 음료수 얼려 먹기.java

---
+++ 이코테/DFS&BFS/음료수 얼려 먹기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] graph = new int[1000][1000];
+
+    public static boolean dfs(int x, int y) {
+        if (x <0 || y < 0 || x >= N || y >= M) {
+            return false;
+        }
+        if (graph[x][y] == 0) {
+            graph[x][y] = 1;
+            dfs(x - 1, y);
+            dfs(x, y - 1);
+            dfs(x + 1, y);
+            dfs(x, y + 1);
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (dfs(i, j)) {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
