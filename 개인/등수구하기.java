8fb059db108433ae7733084d7b825094388b177f
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-12-20 21:20:48 +0900

    Create 등수구하기.java

---
+++ 개인/등수구하기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public ArrayList<Integer> solution(int N, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    num++;
+                }
+            }
+            answer.add(1+num);
+            num = 0;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int x : main.solution(N, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
