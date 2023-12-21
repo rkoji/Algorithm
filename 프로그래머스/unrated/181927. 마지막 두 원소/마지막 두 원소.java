d90a977f3878bba8706ddeeacd7b2ef2b91bb8f1
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-12-21 20:29:11 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length+1];
-        for(int i = 0 ; i < num_list.length ; i++){
+        
+        for (int i = 0; i < num_list.length; i++) {
             answer[i] = num_list[i];
         }
         
-        int x = num_list[num_list.length -2];
-        int y = num_list[num_list.length -1];
-        
-        answer[answer.length -1 ] = y > x ? y - x : y *2;
-        
+        if(num_list[num_list.length-1] > num_list[num_list.length-2]){
+            answer[answer.length-1] =  num_list[num_list.length - 1] - num_list[num_list.length - 2];
+        }else{
+            answer[answer.length-1] = num_list[num_list.length-1] * 2;
+        }
         return answer;
     }
 }
