05a2c5791c2f56ea9217a5113f140f498afeb281
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-12-23 12:14:25 +0900

    [level unrated] Title: ad 제거하기, Time: 0.62 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public ArrayList<String> solution(String[] strArr) {
+        ArrayList<String> answer = new ArrayList<String>();
+        for(int i = 0 ; i < strArr.length ; i++){
+            if(!strArr[i].contains("ad")){
+                answer.add(strArr[i]);
+            }
+        }
+        return answer;
+    }
+}
