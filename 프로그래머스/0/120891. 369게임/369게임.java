32450fba3e5d9737d184f078b6be994e94f85ed4
jiyeon <101394751+rkoji@users.noreply.github.com>
2024-03-05 16:52:45 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        int num = 0;
+        while(order > 0){
+            num = order % 10;
+            if(num == 3 || num == 6 || num == 9){
+                answer++;
+            }
+            order /= 10;
+        }
+        return answer;
+    }
+}
