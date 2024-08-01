8144ce41ed262f5ce46307e85a1c0265d2ee391d
jiyeon <101394751+rkoji@users.noreply.github.com>
2024-08-01 10:04:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,10 +4,9 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         while(n > 0){
-            answer += n % 10;
-            n /= 10;      
+            answer +=  n %10;
+            n /= 10;
         }
-        
         return answer;
     }
 }
