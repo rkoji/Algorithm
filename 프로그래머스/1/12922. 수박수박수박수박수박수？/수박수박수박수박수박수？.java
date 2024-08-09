2c8e9f50912815ae7490f7fb4d3e5124d24dd4f7
jiyeon <101394751+rkoji@users.noreply.github.com>
2024-08-09 10:30:57 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 54.81 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        for(int i = 0 ; i < n ; i++){
-            if(i % 2 != 0){
-                answer += "박";
+        for(int i = 0 ; i <n ; i++){
+            if( i % 2 == 0){
+                answer = answer + "수";
             }else{
-                answer += "수";
+                answer = answer + "박";
             }
         }
         return answer;
