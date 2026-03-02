c30edb382d1c00dc698fc97acabc7297b7eadf84
jiyeon <101394751+rkoji@users.noreply.github.com>
2026-03-02 20:30:41 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        for (int i = 1; ; i++) { 
+            if ((6 * i) % n == 0) {
+                return i; 
+            }
+        }
+    }
+}
