c55576e2cd3b7a65fbd73ad3fc9f8d9c764a4fa5
jiyeon <101394751+rkoji@users.noreply.github.com>
2026-04-26 11:39:42 +0900

    [level 1] Title: 추억 점수, Time: 2.12 ms, Memory: 107 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < name.length; i++){
+            map.put(name[i], yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++){
+            int sum = 0;
+            for(int j = 0; j < photo[i].length; j++){
+                String person = photo[i][j];
+                
+                if(map.containsKey(person)){
+                    sum += map.get(person);
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
